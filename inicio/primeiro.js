d7fd077177969745f15e269eb65a7512e672d17b
Alexandro B S <jornalsinal@gmail.com>
2025-03-24 01:11:24 -0300

    Fundamentos de Programação usando javascript

---
+++ inicio/primeiro.js
@@ -0,0 +1,8 @@
+//Fundamentos de Programação, usando javascript
+
+console.log("Hello World 1!")
+console.log("Hello World 2!")
+console.log("Hello World 3!")
+
+//sentenças de código
+//blocos de código
