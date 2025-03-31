4e9dea9478d9db5da803303de123b3802c8cbb60
Alexandro B S <jornalsinal@gmail.com>
2025-03-30 22:14:14 -0300

    desafio area do circulo

---
+++ inicio/desafio_area.js
@@ -0,0 +1,9 @@
+// PI * raio * raio
+
+const PI = 3.141592;
+const raio = 10;
+let area = PI * raio * raio;
+console.log("A área é de: " + area + " m2.");
+
+area = Math.PI * raio * raio;
+console.log("A área é de: " + area + " m2.");
