a5486e630b7997601e1f463ccdad65db6a42663c
Alexandro B S <jornalsinal@gmail.com>
2025-06-11 22:52:16 -0300

    template strings parte 2

--- inicio/templateString.js
+++ inicio/templateString.js
@@ -5,3 +5,8 @@ const template = `
     ${nome}!`
 console.log(concatenacao, template)
 
+//expressoes...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
